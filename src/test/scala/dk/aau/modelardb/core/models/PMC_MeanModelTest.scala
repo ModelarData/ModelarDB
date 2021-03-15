abd7f36270ba10498826b39c8ae2b611aa80a794
Soeren Kejser Jensen <devel@kejserjensen.dk>
2021-03-15 12:03:56 +0100

    Fix [warn] in test due to use of var instead of val

--- src/test/scala/dk/aau/modelardb/core/models/PMC_MeanModelTest.scala
+++ src/test/scala/dk/aau/modelardb/core/models/PMC_MeanModelTest.scala
@@ -9,12 +9,12 @@ import collection.JavaConverters._
 class PMC_MeanModelTest extends AnyFunSuite with Matchers {
 
     test("Initialize") {
-        var d1 = new DataPoint(1, 123L, 13.0f)
-        var d2 = new DataPoint(2, 124L, 12.0f)
-        var d3 = new DataPoint(3, 125L, 14.0f)
-        var array = Array(d1, d2, d3)
-        var list = List(array)
-        var model = new PMC_MeanModel(12, 10, 10)
+        val d1 = new DataPoint(1, 123L, 13.0f)
+        val d2 = new DataPoint(2, 124L, 12.0f)
+        val d3 = new DataPoint(3, 125L, 14.0f)
+        val array = Array(d1, d2, d3)
+        val list = List(array)
+        val model = new PMC_MeanModel(12, 10, 10)
         model.initialize(list.asJava)
         model.length() should equal (1)
     }
